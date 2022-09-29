cfd370895a9a7793f6f6ecbc8ffea28a744502bd
Cere8ellum <101316713+Cere8ellum@users.noreply.github.com>
2022-09-29 11:31:59 +0300

    Update server.js

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -3,14 +3,17 @@ export const server = (done) => {
     server: {
       // Директория рабочих файлов  
       baseDir: `${app.path.build.html}`,
+      // Открывать директорию, не index.html
+      // directory: true,
+      https: false
     },
     // Убрать сообщения в браузере (можно изменить на true)
     notify: false,
-    port: 3010,
+    port: 3025,
     // Stop the browser from automatically opening
     open: false,
     // After it browser running
-    startPath: "/index.html", 
+    // startPath: "/index.html",
     browser: 'chrome',
     tunnel: true
   });
