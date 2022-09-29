31bd89619003ee8c5b538fb60fc6f5aba01a2705
Cere8ellum <101316713+Cere8ellum@users.noreply.github.com>
2022-09-29 09:43:56 +0300

    Update server.js

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -6,7 +6,12 @@ export const server = (done) => {
     },
     // Убрать сообщения в браузере (можно изменить на true)
     notify: false,
-    // Порт локального сервера
-    port: 3000,
+    port: 3010,
+    // Stop the browser from automatically opening
+    open: false,
+    // After it browser running
+    startPath: "/index.html", 
+    browser: 'chrome',
+    tunnel: true
   });
 }
